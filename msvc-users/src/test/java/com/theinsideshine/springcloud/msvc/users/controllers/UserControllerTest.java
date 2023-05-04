4693143cfa27e997b92051c2de1531e855bb4fd1
theinsideshine <educacion.ta@gmail.com>
2023-05-04 09:47:05 -0300

    add video junit

--- msvc-users/src/test/java/com/theinsideshine/springcloud/msvc/users/controllers/UserControllerTest.java
+++ msvc-users/src/test/java/com/theinsideshine/springcloud/msvc/users/controllers/UserControllerTest.java
@@ -3,7 +3,6 @@ package com.theinsideshine.springcloud.msvc.users.controllers;
 
 import com.theinsideshine.springcloud.msvc.users.Datas;
 import com.theinsideshine.springcloud.msvc.users.models.User;
-import com.theinsideshine.springcloud.msvc.users.repositories.UserRepository;
 import com.theinsideshine.springcloud.msvc.users.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -11,22 +10,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.*;
-
-import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
